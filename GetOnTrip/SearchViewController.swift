adbd1da5a8ccd700914f1b085c729c3268d26071
zhenkunew123 <614451892@qq.com>
2015-11-05 09:50:45 +0800

    收藏页面kvo

--- GetOnTrip/SearchViewController.swift
+++ GetOnTrip/SearchViewController.swift
@@ -38,6 +38,8 @@ class SearchViewController: UISearchController, UISearchBarDelegate, UITableView
         
     }
     
+    
+    
     init() {
         super.init(searchResultsController: searchResult)
         searchResultsUpdater = searchResult
