f1b77e0c80b1b9f5cc33c846eecf2097fa4c21f5
zhenkunew123 <614451892@qq.com>
2015-11-18 13:39:21 +0800

    消息设置更改

--- GetOnTrip/MessageViewController.swift
+++ GetOnTrip/MessageViewController.swift
@@ -41,11 +41,12 @@ class MessageViewController: MenuViewController, UITableViewDataSource, UITableV
         view.addSubview(tableView)
         
         tableView.backgroundColor = UIColor.whiteColor()
-        tableView.ff_AlignInner(ff_AlignType.TopLeft, referView: view, size: CGSizeMake(view.bounds.width, view.bounds.height - 64), offset: CGPointMake(0, 64))
+        tableView.ff_AlignInner(ff_AlignType.TopLeft, referView: view, size: CGSizeMake(view.bounds.width, view.bounds.height - 44), offset: CGPointMake(0, 44))
         tableView.dataSource = self
         tableView.delegate = self
         tableView.separatorStyle = UITableViewCellSeparatorStyle.None
         tableView.registerClass(MessageTableViewCell.self, forCellReuseIdentifier: "Message_Cell")
+        tableView.registerClass(SystemTableViewCell.self, forCellReuseIdentifier: "SystemTableView_Cell")
 //        loadFeedBackHistory()
     }
     
@@ -97,12 +98,27 @@ class MessageViewController: MenuViewController, UITableViewDataSource, UITableV
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Message_Cell", forIndexPath: indexPath) as! MessageTableViewCell
-        cell.message = messageLists[indexPath.row] as MessageList
-        return cell
+        
+        let mes = messageLists[indexPath.row] as MessageList
+        
+        if mes.type == "1" {
+            let cell = tableView.dequeueReusableCellWithIdentifier("SystemTableView_Cell", forIndexPath: indexPath) as! SystemTableViewCell
+            cell.message = mes
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCellWithIdentifier("Message_Cell", forIndexPath: indexPath) as! MessageTableViewCell
+            cell.message = mes
+            return cell
+        }
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        let mes = messageLists[indexPath.row] as MessageList
+        if mes.type == "1" {
+            return SystemTableViewCell.messageWithHeight(mes)
+        }
+        
         return 75
     }
     
@@ -195,7 +211,7 @@ class MessageTableViewCell: UITableViewCell {
     lazy var restoreImageView: UIImageView = UIImageView()
     
     /// 设置底线
-    lazy var baseline: UIView = UIView(color: UIColor.whiteColor(), alphaF: 0.3)
+    lazy var baseline: UIView = UIView(color: SceneColor.shallowGrey, alphaF: 0.3)
     
     var message: MessageList? {
         didSet {
@@ -251,36 +267,50 @@ class SystemTableViewCell: UITableViewCell {
     /// 头像
     lazy var iconView: UIImageView = UIImageView()
     /// 系统回复
-    lazy var restorePerson: UILabel = UILabel(color: UIColor.whiteColor(), title: "系统消息", fontSize: 12, mutiLines: false)
+    lazy var restorePerson: UILabel = UILabel(color: SceneColor.frontBlack, title: "系统消息", fontSize: 12, mutiLines: false)
     /// 回复时间
-    lazy var restoreTime: UILabel = UILabel(color: UIColor(hex: 0x7F7F7F, alpha: 1.0), title: "2天前", fontSize: 9, mutiLines: false)
+    lazy var restoreTime: UILabel = UILabel(color: SceneColor.frontBlack, title: "2天前", fontSize: 9, mutiLines: false)
     /// 标题
-    lazy var title: UILabel = UILabel(color: UIColor.whiteColor(), title: "周天赞自己，升级正能量！", fontSize: 12, mutiLines: false)
+    lazy var title: UILabel = UILabel(color: SceneColor.frontBlack, title: "周天赞自己，升级正能量！", fontSize: 12, mutiLines: false)
     /// 副标题
-    lazy var subTitle: UILabel = UILabel(color: UIColor.whiteColor(), title: "给负能量除以二，让好心情翻一番。来途知查看你的爱心话题，推荐有趣的话题，传递周一正能量能量！查看你的爱心话题，推荐有趣的话题，传递周一正能量能量能量能能量", fontSize: 12, mutiLines: false)
+    lazy var subTitle: UILabel = UILabel(color: SceneColor.frontBlack, title: "给负能量除以二，让好心情翻一番。来途知查看你的爱心话题，推荐有趣的话题，传递周一正能量能量！查看你的爱心话题，推荐有趣的话题，传递周一正能量能量能量能能量", fontSize: 12, mutiLines: false)
     /// 图片
     lazy var restoreImageView: UIImageView = UIImageView()
     
     /// 设置底线
-    lazy var baseline: UIView = UIView(color: UIColor.whiteColor(), alphaF: 0.3)
+    lazy var baseline: UIView = UIView(color: SceneColor.shallowGrey, alphaF: 0.3)
     
     var message: MessageList? {
         didSet {
             iconView.sd_setImageWithURL(NSURL(string: message!.avatar), placeholderImage: PlaceholderImage.defaultSmall)
-            restorePerson.text = message?.content
             restoreTime.text = message?.create_time
             restoreImageView.sd_setImageWithURL(NSURL(string: message!.image), placeholderImage: PlaceholderImage.defaultSmall)
             
             let getWidth = CGRectGetMaxX(restorePerson.frame) + 8
-            if message?.image == "" {
-                
-                title.preferredMaxLayoutWidth = bounds.width - (getWidth + 10)
-                subTitle.preferredMaxLayoutWidth = title.preferredMaxLayoutWidth
+            title.text = message?.title
+            subTitle.text = message?.content
+            if message!.systemMesIsIcon {
+                restoreImageView.removeFromSuperview()
+                title.preferredMaxLayoutWidth = UIScreen.mainScreen().bounds.width - (getWidth + 10)
+                subTitle.preferredMaxLayoutWidth = UIScreen.mainScreen().bounds.width - (getWidth + 10)
             } else {
                 title.preferredMaxLayoutWidth = bounds.width - (getWidth + iconView.bounds.width + 10 + 18)
-                subTitle.preferredMaxLayoutWidth = title.preferredMaxLayoutWidth
+                subTitle.preferredMaxLayoutWidth = bounds.width - (getWidth + iconView.bounds.width + 10 + 18)
             }
         }
+    }
+    
+    class func messageWithHeight(mes: MessageList) -> CGFloat {
+        var w: CGFloat = 114
+        var h: CGFloat = 24
+        if mes.systemMesIsIcon {
+            w = UIScreen.mainScreen().bounds.width - CGFloat(114) - CGFloat(105)
+        } else {
+            w = UIScreen.mainScreen().bounds.width - CGFloat(114) - CGFloat(9)
+        }
+        h = h + mes.title.sizeofStringWithFount1(UIFont.systemFontOfSize(12), maxSize: CGSizeMake(w, CGFloat.max)).height +
+                mes.content.sizeofStringWithFount1(UIFont.systemFontOfSize(12), maxSize: CGSizeMake(w, CGFloat.max)).height
+        return max(h, 72)
     }
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
@@ -294,10 +324,13 @@ class SystemTableViewCell: UITableViewCell {
         addSubview(restorePerson)
         addSubview(restoreImageView)
         
+        title.numberOfLines = 0
+        subTitle.numberOfLines = 0
+        
         iconView.clipsToBounds = true
         iconView.layer.cornerRadius = iconView.bounds.width * 0.5
         
-        iconView.ff_AlignInner(ff_AlignType.CenterLeft, referView: self, size: CGSizeMake(35, 35), offset: CGPointMake(9, 0))
+        iconView.ff_AlignInner(ff_AlignType.TopLeft, referView: self, size: CGSizeMake(35, 35), offset: CGPointMake(10, 19))
         restorePerson.ff_AlignHorizontal(ff_AlignType.TopRight, referView: iconView, size: nil, offset: CGPointMake(7, 0))
         restoreTime.ff_AlignHorizontal(ff_AlignType.BottomRight, referView: iconView, size: nil, offset: CGPointMake(7, 0))
         restoreImageView.ff_AlignInner(ff_AlignType.CenterRight, referView: self, size: CGSizeMake(77, 58), offset: CGPointMake(-9, 0))
